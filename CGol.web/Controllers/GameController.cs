6157bf6c0cc226341c3d1b9e995a0cf371cf7599
Shawn Wallace <shawnewallace@gmail.com>
2015-03-27 12:37:56 -0400

    adding services for game

---
+++ CGol.web/Controllers/GameController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using CGol.lib;
+using CGol.web.Models;
+using Ninject;
+
+namespace CGol.web.Controllers
+{
+	public class GameController : ApiController
+	{
+		public ICGolGameCreator Creator
+		{
+			get { return _creator ?? (_creator = new CGolGameCreator()); }
+			set { _creator = value; }
+		}
+		private ICGolGameCreator _creator;
+
+
+		// POST: api/Game
+		public OkNegotiatedContentResult<GameModel> Post([FromBody] NewGameModel model)
+		{
+			Creator.Width = model.Width;
+			Creator.Height = model.Height;
+			Creator.FillFactor = model.FillFactor;
+			var rawGame = Creator.Execute();
+
+			var game = new GameModel(rawGame) { FillFactor = model.FillFactor };
+			return Ok(game);
+		}
+
+		// PUT: api/Game/5
+		public OkNegotiatedContentResult<GameModel> Put([FromBody] GameModel game)
+		{
+			ICGolGame domainGame = new CGolGame();
+			domainGame = game.ApplyTo(domainGame);
+
+			domainGame.Tick();
+
+			return Ok(new GameModel(domainGame));
+		}
+	}
+}
