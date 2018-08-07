8f6c5a37acba5e1a6346f0b6c3ddad25cb0af273
Shawn Wallace <shawn@the-wallaces.net>
2018-08-07 10:40:47 -0400

    using service to get generators

--- dotnetcore/conways-game-of-life.ng/src/app/game.service.ts
+++ dotnetcore/conways-game-of-life.ng/src/app/game.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Game } from './game';
 import { BoardGenerator } from './board_generator';
 
 import { BOARD_GENERATORS } from './mock_generators';
+// tslint:disable-next-line:import-blacklist
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class GameService {
 
-  constructor() { }
+  constructor(private http:  HttpClient) { }
 
   getGenerators(): Observable<BoardGenerator[]> {
     return Observable.of(BOARD_GENERATORS);
